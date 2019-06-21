1e811329e1ce230e6f4643ff809791ccf81a7375
rahulglobo <rahul.dubey@globocom.info>
2019-06-21 14:45:42 +0530

    check

--- src/main/java/com/globocom/controller/UploadController.java
+++ src/main/java/com/globocom/controller/UploadController.java
@@ -1,6 +1,7 @@
 package com.globocom.controller;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
@@ -12,6 +13,9 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -66,10 +70,11 @@ public class UploadController {
 				}
 				boolean previewFileStatus = false;
 				if (null != files && files.size() > 0) {
+					if("1008".equals(contentType)){
+						
+					}else{
 					for (MultipartFile multipartFile : files) {
 						String fileName = "";
-						
-						
 						try {
 							fileName = multipartFile.getOriginalFilename();
 							System.out.println("contentDetail fileName: :" + fileName);
@@ -174,7 +179,7 @@ public class UploadController {
 							e.printStackTrace();
 						}
 					}
-					
+				}
 					
 				}
 			} catch (Exception e) {
@@ -287,6 +292,37 @@ public class UploadController {
 			}
 		}
 		return flag;
+
+	}
+	
+	public static void readExcel(String filepath) throws IOException{
+
+		FileInputStream file = new FileInputStream(new File(filepath)); 
+		XSSFWorkbook workbook = new XSSFWorkbook(file); 
+		XSSFSheet sheet = workbook.getSheetAt(0); 
+	    Row row;
+	    for(int i=1; i<=sheet.getLastRowNum(); i++){  //points to the starting of excel i.e excel first row
+	        row = (Row) sheet.getRow(i);  //sheet number
+
+	            String title;
+				if( row.getCell(0)==null) { title = "null"; }
+	            else title= row.getCell(0).toString();
+
+	               String link;
+				if( row.getCell(1)==null) { link = "null";}  //suppose excel cell is empty then its set to 0 the variable
+	               else link = row.getCell(1).toString();   //else copies cell data to name variable
+
+	               String description;
+				if( row.getCell(2)==null) { description = "null";   }
+	               else  description   = row.getCell(2).toString();
+				
+				String previewPath;
+				if( row.getCell(3)==null) { previewPath = "null";   }
+	               else  previewPath   = row.getCell(3).toString();
+				
+		System.out.println("title:"+title+" name:"+link+" address:"+description+" previewPath:"+previewPath);
+	    }
+		file.close();
 
 	}
 }
